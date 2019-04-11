5a92de34c317227a86724891a12b84ab5f0879b0
nascVi <vaiiho.nascimbene@gmail.com>
2019-04-11 19:39:23 +0200

    Après correction

--- Semaine1/Exercice1.1.3.py
+++ Semaine1/Exercice1.1.3.py
@@ -1,21 +1,6 @@
 def estPalindrome( s ):
-
-    s = s.replace(' ', '').lower()
+    s = s.replace(' ', '')
     return s == s [::-1]
-
-    i = 0
-    j = len(s) - 1
-
-    while i < j:
-
-        if s[i] != s[j]:
-
-            return False
-
-        i+=1
-        j-=1
-
-    return True
 
 
 M = input( "Checkez si palindrome \n " )

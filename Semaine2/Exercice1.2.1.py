0f187fbd0af98d79a6732a339bee4dfba1aba9d8
nascVi <vaiiho.nascimbene@gmail.com>
2019-04-13 08:25:02 +0200

    initial Semaine2

---
+++ Semaine2/Exercice1.2.1.py
@@ -0,0 +1,12 @@
+'''
+Exercice 1.2.1: Calculer la factorielle d'un nombre
+On souhaite calculer factoriel d'un nombre a. Factoriel s'exprime comme le produit des entiers jusqu'à a inclus : a! = 1 x 2 x 3 ... x a
+
+Définir un programme pour calculer la factorielle d'un nombre a . Quel est le nombre d'opérations à effectuer?
+
+Discutons en ensemble sur le forum: 
+https://forum.alyra.fr/t/exercice-1-2-1-calculer-la-factorielle-dun-nombre/71
+'''
+
+import math
+

e41dc8bacbf7196a7e5543d75dfe47ef448bd542
nascVi <vaiiho.nascimbene@gmail.com>
2019-04-15 11:24:41 +0200

    initial Exercise for py semaine 2

--- Semaine2/Exercice1.2.1.py
+++ Semaine2/Exercice1.2.1.py
@@ -4,9 +4,37 @@ On souhaite calculer factoriel d'un nombre a. Factoriel s'exprime comme le produ
 
 Définir un programme pour calculer la factorielle d'un nombre a . Quel est le nombre d'opérations à effectuer?
 
-Discutons en ensemble sur le forum: 
+Discutons en ensemble sur le forum:
 https://forum.alyra.fr/t/exercice-1-2-1-calculer-la-factorielle-dun-nombre/71
 '''
 
+import os
 import math
 
+# Tant que n n'est pas N la console propose l'entrée d'un nombre
+n = -1
+
+while n<0:
+    n = input("\n Calculer la valeur factorielle d'un nombre de votre choix:  ")
+
+    try:
+        # Soit n la valeur entrée par l'utilisateur convertie pour le calcule
+        n = int(n)
+
+        # Prise en compte de la saisie d'une valeur autre qu'un N
+    except ValueError:
+        print("Le calcul sera effectué à partir d'un entier naturel.")
+        n = -1
+    continue
+
+# initialisation des variables pour le nombre d'opérations nécessaires et pour la factorielle
+i = 1
+f = 1
+
+while i <= n :
+    f = f * i
+    i = i+1
+    # nombre d'opérations effectuées
+    print(i)
+
+print("La factorielle pour ", n, "est: ", f)

11235d91b2d8d229e1a779a54a8b4e73122cb76b
nascVi <vaiiho.nascimbene@gmail.com>
2019-04-19 07:01:10 +0200

    1.2.2 comments

--- Semaine2/Exercice1.2.2.py
+++ Semaine2/Exercice1.2.2.py
@@ -2,11 +2,11 @@
 Exercice 1.2.2 : Obtenir les meilleurs pourboires
 On définit un ensemble de transactions avec leur taille (en octets) et leur pourboire (en satoshis) associés :
 
-Taille (octets)
+Taille en octets:
 
 2000 6000 800 700 1200 1000 1300 600
 
-Pourboire (satoshis)
+Pourboire en satoshis:
 
 13000 9000 2000 1500 3500 2800 5000 1500
 
